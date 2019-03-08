6e4b6023266a04b23c9efb91b37cf8e48c1fe98b
Leigh <lsteiner@locus.co>
2019-03-08 17:21:20 -0500

    login working but not really persisted yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
 class App extends Component {
+  state = {
+    user: "",
+    name: "", 
+    password: "",
+  }
+
+  onChange = (evt) => {
+    this.setState({[evt.target.name] : evt.target.value})
+  }
+  login = () => {
+    let username = this.state.name
+    let password = this.state.password
+    if(username && password){
+      axios.post('http://127.0.0.1:8000/api/v1/rest-auth/login/', {username, password})
+      .then(res => {
+        console.log('yay!', res.data)
+        document.cookie = `token=${res.data.key}`
+        this.setState({user: res.data})
+      })
+      .catch(err => console.log('oh no', err))
+    }
+  }
+
+ getCookie = (name) => {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim()
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+  seeBlogs = () => {
+    const cookie = this.getCookie('token')
+    axios.get('http://127.0.0.1:8000/api/v1/1/', { headers: { Authorization: `Token  ${cookie}` }})
+    .then(res => console.log('yay!!!', res.data))
+    .catch(err => console.log('boo', err))
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { !this.state.user && 
+        <div>
+          <label>name </label><input onChange={this.onChange} name="name"/>
+          <label>password </label><input onChange={this.onChange} name="password" />
+          <button onClick={this.login}> login </button>
+        </div>
+        }
+        {
+          this.state.user &&
+          <button onClick={this.seeBlogs}>see blogs?</button>
+        }
       </div>
     );
   }
